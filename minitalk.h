da8e0829db0c579771f3b6e223aac5607ee02d1d
SaidKarim-web <sirsaidkarim@gmail.com>
2024-01-24 19:05:45 +0100

    Modify the header file

--- minitalk.h
+++ minitalk.h
@@ -6,16 +6,21 @@
 /*   By: skarim <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/21 17:16:11 by skarim            #+#    #+#             */
-/*   Updated: 2024/01/22 18:52:25 by skarim           ###   ########.fr       */
+/*   Updated: 2024/01/24 19:02:03 by skarim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <signal.h>
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <signal.h>
 
 int		ft_strlen(char *str);
 void	ft_error_msg(char *str);
 int		ft_atoi(char *str);
 void	ft_putchar(char c);
 void	ft_putnbr(int n);
+
+#endif
