3acb0b825c558e794db889e252a5c161385bc6b4
SaidKarim-web <sirsaidkarim@gmail.com>
2024-01-24 19:09:32 +0100

    Modify the client file

--- Mandatory/client.c
+++ Mandatory/client.c
@@ -6,7 +6,7 @@
 /*   By: skarim <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/21 12:39:43 by skarim            #+#    #+#             */
-/*   Updated: 2024/01/22 22:24:36 by skarim           ###   ########.fr       */
+/*   Updated: 2024/01/24 16:10:17 by skarim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,43 +14,48 @@
 
 int	ft_check_args(int ac, char **av)
 {
-
 	if (ac != 3)
-		ft_error_msg("Too many arguments!");
+		ft_error_msg("Invalid arguments!");
 	return (ft_atoi(av[1]));
 }
 
-void	ft_send_message(char *msg, int pid)
+void	ft_send_char(char c, int pid)
 {
-	int	i;
-	int	shift_nbr;
-	
-	i = 0;
-	if (msg[0] == '\0')
-		ft_error_msg("The message is empty!");
-	while (msg[i])
+	int	shift_number;
+
+	shift_number = 7;
+	while (shift_number >= 0)
 	{
-		shift_nbr = 7;
-		while (shift_nbr >= 0)
+		if ((c >> shift_number) % 2 != 0)
 		{
-			if ((msg[i] >> shift_nbr) % 2 != 0)
-				kill(pid, SIGUSR1);
-			else
-				kill(pid, SIGUSR2);
-			usleep(500);
-			shift_nbr--;
+			if (kill(pid, SIGUSR1) == -1)
+				ft_error_msg("send a message fails!");
 		}
-		i++;
+		else
+		{
+			if (kill(pid, SIGUSR2) == -1)
+				ft_error_msg("send a message fails!");
+		}
+		usleep(500);
+		shift_number--;
 	}
 }
 
 int	main(int ac, char **av)
 {
 	int	pid;
+	int	i;
 
 	pid = ft_check_args(ac, av);
-	if (pid == -1 || pid == 0)
+	if (pid <= 0)
 		ft_error_msg("Invalid pid!");
-	ft_send_message(av[2], pid);
+	if (av[2][0] == '\0')
+		ft_error_msg("The message is empty!");
+	i = 0;
+	while (av[2][i])
+	{
+		ft_send_char(av[2][i], pid);
+		i++;
+	}
 	return (0);
 }
